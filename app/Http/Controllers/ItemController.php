182e0211d325d172bf1b1b2181ea2c4ad43d7134
lazlanrafar <lazlanrafar@gmail.com>
2024-11-28 16:28:29 +0800

    feat(project): document management

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Item;
 use App\Models\ItemHistory;
 
+use App\Models\PurchaseOrderItem;
+use App\Models\SalesOrderItem;
+
 class ItemController extends Controller
 {
     /**
@@ -62,5 +65,28 @@ class ItemController extends Controller
         $item->update($data);
 
         return redirect()->route('item.index')->with('success', 'Successfully edit item');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        // check if item is used in purchase order
+        $purchase_order_item = PurchaseOrderItem::where('item_id', $id)->first();
+        if ($purchase_order_item) {
+            return redirect()->route('item.index')->with('error', 'Item is used in purchase order');
+        }
+
+        // check if item is used in sales order
+        $sales_order_item = SalesOrderItem::where('item_id', $id)->first();
+        if ($sales_order_item) {
+            return redirect()->route('item.index')->with('error', 'Item is used in sales order');
+        }
+
+        $item = Item::find($id);
+        $item->delete();
+
+        return redirect()->route('item.index')->with('success', 'Successfully delete item');
     }
 }
